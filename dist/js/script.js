fb35bfa35c2a9d6226908b5fb35c34966691e41d
Преображенский Денис <30519612+sailor-den@users.noreply.github.com>
2020-08-23 11:25:19 +0300

    Добавил адаптив

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,14 +5,9 @@ $(document).ready(function($) {
 	$('.catalog__menu .menu__item').click(function(e) {
 		e.preventDefault();
 
-        let blockRep = $(this).parents('.catalog').find('.catalog__group');
-        console.log(blockRep)
         category = $(this).data('group'),
-        console.log(category)
         prevCategory = $('.catalog__menu .menu__item.menu__item_active'),
-        console.log(prevCategory)
         prevCategoryId = $('.catalog__menu .menu__item.menu__item_active').data('group');
-        console.log(prevCategoryId)
 
 		prevCategory.removeClass('menu__item_active');
 		$('.catalog__menu .menu__item[data-group='+category+']').addClass('menu__item_active');
